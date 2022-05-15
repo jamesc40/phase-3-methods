99e8c80cc54ecefbe1e8f53e20810a629d96eb30
James Coffman <jamesc4023@gmail.com>
2022-05-15 18:46:31 -0400

    commit tree bce1d04c77273eb487f421564635dcd526080117

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+  puts 'Hello, programmer!'
+end
+
+def greet name
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default name = 'programmer'
+  puts "Hello, #{name}!"
+end
+
+def add num, num2
+  num + num2
+end
+
+def halve num
+  if num.class != Integer && num.class != Float
+    return nil
+  end
+
+  num / 2
+end
